7aabe771e5a4454f260a9e493b7e426aadc2f706
Oneshot369 <joshcpeck4@gmail.com>
2023-07-04 11:22:21 -0500

    first commit

---
+++ src/main/java/com/josh/port/TestController.java
@@ -0,0 +1,12 @@
+package com.josh.port;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String Test(){
+        return "Hello World";
+    }
+}
