c34f0582438ea9ab0eb0598a88c47f18f083ce9e
Oneshot369 <joshcpeck4@gmail.com>
2023-07-08 19:32:57 -0500

    added api, and shop controllers

---
+++ src/main/java/com/josh/port/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.josh.port.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/")
+    public String getApiPage() {
+        return "api";
+    }
+}
--- src/main/java/com/josh/port/controller/HomeController.java
+++ src/main/java/com/josh/port/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.josh.port;
+package com.josh.port.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
